e6d1ade3be06520a93af9518abccc8ba06cde49f
Kagwi <kelvinmwas8@gmail.com>
2025-05-17 16:12:29 +0300

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import ServiceCard from '../components/ServiceCard';
 import TestimonialCard from '../components/TestimonialCard';
