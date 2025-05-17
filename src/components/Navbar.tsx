880b032ccf1ed25a219c57157f92a2d770510700
Kagwi <kelvinmwas8@gmail.com>
2025-05-17 12:15:56 +0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -59,7 +59,7 @@ function Navbar({ scrolled }: NavbarProps) {
         <Link to="/" className="flex items-center space-x-2 text-primary-600">
           {/* Logo image from /public/assets/logo.png */}
           <img
-            src="/main/public/Neonsol%20Logo%20Final.PNG"}
+            src="/Kagwi/Neonsol-Creatives/blob/main/public/Neonsol%20Logo%20Final.PNG"}
             alt="Neonsol Logo"
             className="h-8 w-auto"
           />
