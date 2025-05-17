c3fbff0bcb5bf8286597e89184baf1d0d82d43f7
Kagwi <kelvinmwas8@gmail.com>
2025-05-17 16:23:57 +0300

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -84,7 +84,7 @@ import Button from '../components/Button';
 
 function AboutPage() {
   // State for count up animation
-  var [years, setYears] = useState(0);
+  const [years, setYears] = useState(0);
   const [projects, setProjects] = useState(0);
   const [team, setTeam] = useState(0);
   const [satisfaction, setSatisfaction] = useState(0);
