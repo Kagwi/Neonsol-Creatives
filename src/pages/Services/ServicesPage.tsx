a9f574551d94fa30696cfdc07c057390c213004d
Kagwi <kelvinmwas8@gmail.com>
2025-05-19 16:58:08 +0300

    Update ServicesPage.tsx

--- src/pages/Services/ServicesPage.tsx
+++ src/pages/Services/ServicesPage.tsx
@@ -12,7 +12,7 @@ function ServicesPage() {
    {/* Hero Section */}
   <section
     className="pt-32 pb-20 bg-secondary-950 bg-blend-overlay bg-cover bg-center text-white"
-    style={{ backgroundImage: "url('/images/services-hero.jpg')" }}
+    style={{ backgroundImage: "url('/public/pexels-david-geib-1265112-3220850.jpg')" }}
   >
   <div className="container mx-auto px-4">
     <div className="max-w-3xl">
