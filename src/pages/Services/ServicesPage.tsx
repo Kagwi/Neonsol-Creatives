72df342369daccf02b35c8731c414af239757077
Kagwi <kelvinmwas8@gmail.com>
2025-05-19 17:15:42 +0300

    Update ServicesPage.tsx

--- src/pages/Services/ServicesPage.tsx
+++ src/pages/Services/ServicesPage.tsx
@@ -12,7 +12,11 @@ function ServicesPage() {
    {/* Hero Section */}
   <section
   className="pt-32 pb-20 bg-secondary-950 bg-blend-overlay bg-cover bg-center text-white"
-  style={{ backgroundImage: "url('/pexels-david-geib-1265112-3220850.jpg')" }}    
+  style={{ 
+         backgroundImage: 'url("https://raw.githubusercontent.com/Kagwi/Neonsol-Creatives/refs/heads/main/public/pexels-david-geib-1265112-3220850.jpg")',
+         backgroundPosition: 'center center',
+         backgroundAttachment: 'fixed',
+         }}    
   <div className="container mx-auto px-4">
     <div className="max-w-3xl">
       <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold mb-6 animate-slide-up">
